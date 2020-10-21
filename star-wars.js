3917e4a73acb4bb94216479fc7392ee3d3ecd85d
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-21 12:36:13 -0700

    [js] add star-wars object

---
+++ star-wars.js
@@ -0,0 +1,11 @@
+const starWars = {
+    channel: 'Disney +',
+    title: 'Mandalorian',
+    specialLook: true,
+    babyYodaPresent: true,
+    views: 1000000,
+    time: '1:00'
+
+}
+
+console.log(starWars.specialLook);
